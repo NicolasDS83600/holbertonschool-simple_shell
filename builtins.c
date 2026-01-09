7f668982adead61e3d49d8ec9707cd012887e118
NicolasDS83600 <nico836001@outlook.fr>
2026-01-09 20:08:02 +0100

    feat(shell): add builtin_env and print_env functions
    Introduces support for the `env` builtin:
    
    - `builtin_env` checks if the command is exactly "env" with no extra arguments.
    - `print_env` prints all environment variables to stdout.
    - Keeps `builtin_exit` unchanged, preserving its behavior.
    
    This enables the shell to recognize and execute the `env` builtin command.

--- builtins.c
+++ builtins.c
@@ -6,7 +6,7 @@
 * builtin_exit - handles the "exit" builtin command
 * @args: array of command arguments
 *
-* Return: -1 to signal shell_loop to exit, 0 otherwise
+* Return: 1 if exit, 0 otherwise
 */
 int builtin_exit(char **args)
 {
@@ -17,4 +17,39 @@ int builtin_exit(char **args)
 		return (1);
 
 	return (0);
+}
+
+/**
+* builtin_env - handles the env builtin command
+* @args: array of arguments
+*
+* Return: 1 if is "env", 0 otherwise
+*/
+int builtin_env(char **args)
+{
+	if (args == NULL || args[0] == NULL)
+		return (0);
+
+	if (strcmp(args[0], "env") == 0 && args[1] == NULL)
+		return (1);
+
+	return (0);
+}
+
+/**
+* print_env - prints the environnement variable
+* @env: environnement variables
+*/
+void print_env(char **env)
+{
+	int element = 0;
+
+	if (env == NULL)
+		return;
+
+	while (env[element] != NULL)
+	{
+		printf("%s\n", env[element]);
+		element++;
+	}
 }
